175cbf5996d4a8bd725f6b0e0a179f36f8c2e629
ElizabethOnTheBathroomFloor <danil-pavlovz@mail.ru>
2022-03-26 00:03:55 +0300

    FIRST MOVE

---
+++ public/scripts/drag_and_drop.js
@@ -0,0 +1,101 @@
+const default_position = document.getElementById("def_pos"); // selectors - class of position
+const weight_1 = document.getElementById("weight1");
+const weight_2 = document.getElementById("weight2");
+var position_st = document.getElementById("pos_sh");
+const position_0 = document.getElementById("position0");
+
+const position_1_R = document.getElementById("positionR1");
+const position_1_L = document.getElementById("positionL1");
+
+const position_2_R = document.getElementById("positionR2");
+const position_2_L =document.getElementById("positionL2");
+
+const position_3_R = document.getElementById("positionR3");
+const position_3_L = document.getElementById("positionL3");
+
+const position_4_R =document.getElementById("positionR4");
+const position_4_L =document.getElementById("positionL4");
+
+default_position.ondragover = allowDrop;
+position_0.ondragover=allowDrop;
+position_st.ondragover = allowDrop;
+position_1_R.ondragover = allowDrop;
+position_1_L.ondragover=allowDrop;
+position_2_R.ondragover=allowDrop;
+position_2_L.ondragover=allowDrop;
+position_3_R.ondragover=allowDrop;
+position_3_L.ondragover=allowDrop;
+position_4_R.ondragover=allowDrop;
+position_4_L.ondragover=allowDrop;
+function allowDrop(event){
+    event.preventDefault();
+}
+
+weight_1.ondragstart = drag;
+weight_2.ondragstart = drag;
+
+function drag(event){
+    event.dataTransfer.setData('id', event.target.id);
+}
+
+default_position.ondrop = drop;
+position_0.ondrop=drop;
+position_st.ondrop = drop;
+position_1_R.ondrop=drop;
+position_1_L.ondrop=drop;
+position_2_R.ondrop=drop;
+position_2_L.ondrop=drop;
+position_3_R.ondrop=drop;
+position_3_L.ondrop=drop;
+position_4_R.ondrop=drop;
+position_4_L.ondrop=drop;
+
+
+function drop(event){
+    let test1=document.getElementById('weight1');
+    let test2=document.getElementById('weight2');
+    //console.log(test1)
+    let itemId = event.dataTransfer.getData('id');
+    let b1=0;
+    if ((test1.childElementCount === 0) &&   (test2.childElementCount === 0))
+        b1=1;
+    //console.log((event.target.id.slice(-1)));
+    // if (event.target === test1 && event.target === test2){
+    //
+    // }else{
+    //     pos = event.target;}
+
+    let springpenis = document.getElementById("springwropeid");
+    if (test1.firstChild===test2) {
+        event.target.appendChild(document.getElementById("weight2"));
+        springpenis.style.height = String(90 + "px");
+    }
+    else if (test2.firstChild===test1){
+        event.target.appendChild(document.getElementById("weight1"));
+        springpenis.style.height = String(90 + "px");
+
+    }
+    else{
+        // ((test1.parentNode.id === position_st.id) && (test2.parentNode.id !== position_st.id)) ||  ((test1.parentNode.id === position_st.id) && (test2.parentNode.id !== position_st.id)){
+
+
+        event.target.append(document.getElementById(itemId));
+    }
+    if (((position_st.id === test1.parentNode.id) || (position_st.id === test2.parentNode.id)) &&   (test1.childElementCount === 0) &&   (test2.childElementCount === 0) && b1===1){
+        console.log(position_st.id);
+        for (let i = 0; i < 275; i += 0.005) {
+            setTimeout(() =>{
+                springpenis.style.height = String(90 + i + "px");
+            },200)
+            //position_st.style.top = String(445 + i + "px");
+            //console.log(springpenis.style.height);
+        }
+    }
+    if (b1===0 && (test1.childElementCount === 0) &&   (test2.childElementCount === 0))
+        springpenis.style.height = String(365 + "px");
+    if (position_st.childElementCount === 0) {
+        springpenis.style.height = String(90 + "px");
+    }
+
+    //console.log(test1.firstChild);
+}
---
+++ public/scripts/spin_wheel_script.js
@@ -0,0 +1,462 @@
+var looper;
+var degrees = 0;
+
+var right_spring_height = 423;
+var left_spring_height = 422;
+
+var now;
+var t2;
+var flag1=0;
+
+var bol = 1;
+
+var psi1=0;
+var A;
+var psi2=0;
+var elem1 = document.getElementById("right_spring");
+var elem2 = document.getElementById("left_spring");
+
+var Tau;
+var q;
+var weight_radius_1;
+var weight_radius_2;
+
+function rotateAnimation(el,speed){
+
+    if (bol === 1) {
+
+        let newdate = new Date();
+        t2 = newdate.getTime() / 1000 - now;
+        //console.log(A);
+        psi2 = A * Math.exp(-1 * (t2) / Tau) * Math.cos(q * Math.sqrt(2 * 14.4107 * 256 * 0.000001 / (0.00375 + 0.2 * Math.pow(weight_radius_1, 2) + 0.2 * Math.pow(weight_radius_2, 2))) * t2);
+        //console.log(degrees);
+
+        let Delta_psi = psi1 - psi2;
+
+        psi1 = psi2;
+
+
+        left_spring_height = 422 * (1 - Math.sin(degrees * Math.PI / 180) / 5);
+        right_spring_height = 423 * (1 + Math.sin(degrees * Math.PI / 180) / 5);
+
+
+        if (Delta_psi > 0) {
+            // left_spring_height =  Math.exp(-1 * (t2) / 8.66) * Math.cos(8 * 0.66 * (t2)) ;
+            // right_spring_height = Math.exp(-1 * (t2) / 8.66) * Math.cos(8 * 0.66 * (t2));
+            // left_spring_height += Math.abs(Delta_psi) * 1.5 ;
+            // right_spring_height -= Math.abs(Delta_psi) * 1.5;
+
+            elem2.style.height = (String(left_spring_height) + "px");
+            elem1.style.height = (String(right_spring_height) + "px");
+        } else if (Delta_psi < 0) {
+            // left_spring_height = (421 * Math.cos(A) ) *Math.exp(-1 * (t2) / 8.66) * Math.cos(8 * 0.66 * (t2));
+            // right_spring_height = (422 * Math) * Math.exp(-1 * (t2) / 8.66) * Math.cos(8 * 0.66 * (t2));
+            // left_spring_height -= Math.abs(Delta_psi) * 1.5;
+            // right_spring_height += Math.abs(Delta_psi) * 1.5;
+
+            elem2.style.height = (String(left_spring_height) + "px");
+            elem1.style.height = (String(right_spring_height) + "px");
+        }
+
+
+
+        var elem = document.querySelector(el);
+        if (navigator.userAgent.match("Chrome")) {
+            elem.style.WebkitTransform = "rotate(" + degrees + "deg)";
+        } else if (navigator.userAgent.match("Firefox")) {
+            elem.style.MozTransform = "rotate(" + degrees + "deg)";
+        } else if (navigator.userAgent.match("MSIE")) {
+            elem.style.msTransform = "rotate(" + degrees + "deg)";
+        } else if (navigator.userAgent.match("Opera")) {
+            elem.style.OTransform = "rotate(" + degrees + "deg)";
+        } else {
+            elem.style.transform = "rotate(" + degrees + "deg)";
+        }
+
+        degrees = psi2;
+
+
+        let elis=document.getElementById("start");
+
+        el_stop.addEventListener("click", () => {     // КНОПКА СБРОС
+            elis.disabled = false;
+            elis.style.backgroundColor = ('#88CC88');
+
+            bol = 0;
+            degrees = 0
+            right_spring_height = 423;
+            left_spring_height = 422;
+            elem2.style.height = (String(left_spring_height) + "px");
+            elem1.style.height = (String(right_spring_height) + "px");
+
+            el.style.transform = "rotate(" + degrees + "deg)";
+
+        })
+
+        looper = setTimeout('rotateAnimation(\'' + el + '\',' + speed + ')', speed);
+    }else {
+        return 0;
+    }
+
+}
+let el=document.getElementById("start");
+let elem=document.querySelector(".move");
+
+degrees= 0;
+
+var disk1=document.querySelector(".move");
+
+var x,y,x1,y1;
+var flag=0;
+disk1.addEventListener("mousedown", e=>{
+    flag = 1;
+    x=e.x;
+    y=e.y
+})
+
+
+let deg_pred=0;
+
+
+
+// disk1.onmousemove = function (event) {
+//     if (flag === 1) {
+//
+//         x1 = event.x;
+//         y1 = event.y;
+//
+//         left_spring_height = 422 * (1 - Math.sin(degrees * Math.PI / 180) / 5);
+//         right_spring_height = 423 * (1 + Math.sin(degrees * Math.PI / 180) / 5);
+//
+//
+//         if (x1-x > 0)
+//             {if (degrees>-50) {
+//
+//
+//
+//                     // right_spring_height = 423 * Math.cos(degrees*Math.PI/180);
+//                     // left_spring_height = 422 * (1 + Math.cos(degrees*Math.PI/180));
+//                     //left_spring_height = left_spring_height * (Math.cos(0.35 * Math.PI / 180) + Math.tan(degrees * Math.PI / 180) * Math.sin(0.35 * Math.PI / 180));
+//                 //right_spring_height = 2 * 422 - (2 * 422 - right_spring_height) * Math.cos(0.35 * Math.PI / 180) - (2 * 422 - right_spring_height) * Math.tan(degrees * Math.PI / 180) * Math.sin(0.35 * Math.PI / 180);
+//                 //left_spring_height = left_spring_height * Math.cos(degrees * Math.PI/180);
+//                 //right_spring_height = right_spring_height * (2 - Math.cos(degrees * Math.PI/180));
+//
+//
+//
+//                 degrees -= 0.35;
+//
+//             }
+//             }
+//         else if (x1-x < 0){
+//
+//             if (degrees<50) {
+//
+//
+//
+//                 //right_spring_height = right_spring_height * (Math.cos(0.35 * Math.PI/180) - Math.tan(degrees * Math.PI/180) * Math.sin(0.35 * Math.PI/180) );
+//                 //left_spring_height = 2 * 422 - (2 * 422 - left_spring_height) * Math.cos(0.35 * Math.PI / 180) + (2 * 422 - left_spring_height) * Math.tan(degrees * Math.PI / 180) * Math.sin(0.35 * Math.PI / 180);
+//
+//                 //right_spring_height = 423 * (2 - Math.cos(degrees*Math.PI/180));
+//                 // left_spring_height = 422 * Math.cos(degrees*Math.PI/180);
+//                 // //right_spring_height = right_spring_height * Math.cos(degrees * Math.PI/180);
+//                 // right_spring_height = 423 * (1 + Math.cos(degrees * Math.PI/180));
+//                 degrees += 0.35;
+//             }
+//
+//         }
+//
+//
+//         deg_pred=degrees;
+//         console.log("L", left_spring_height);
+//         console.log("R", right_spring_height);
+//         elem1.style.height = (String(right_spring_height) + "px");
+//         elem2.style.height = (String(left_spring_height) + "px");
+//         if(navigator.userAgent.match("Chrome")){
+//                disk1.style.WebkitTransform = "rotate("+degrees+"deg)";
+//          } else if(navigator.userAgent.match("Firefox")){
+//              disk1.style.MozTransform = "rotate("+degrees+"deg)";
+//          } else if(navigator.userAgent.match("MSIE")){
+//              disk1.style.msTransform = "rotate("+degrees+"deg)";
+//          } else if(navigator.userAgent.match("Opera")){
+//              disk1.style.OTransform = "rotate("+degrees+"deg)";
+//          } else {
+//              disk1.style.transform = "rotate("+degrees+"deg)";
+//          }
+//         x = x1;
+//         y = y1;
+//     }}
+
+disk1.addEventListener("mouseup", e => {
+    flag = 0;
+})
+//!!
+let penis = document.querySelector(".positions")
+penis.addEventListener("click", e => {
+    x2 = e.offsetX;
+    y2 = e.offsetY;
+    console.log(y2, "y");
+    console.log(x2, "x");
+})
+
+
+let el_stop = document.getElementById("reset");
+
+first_weight = document.getElementById('weight1');
+second_weight = document.getElementById("weight2");
+//degrees=0;
+if (navigator.userAgent.match("Chrome")) {
+    elem.style.WebkitTransform = "rotate(" + degrees + "deg)";
+} else if (navigator.userAgent.match("Firefox")) {
+    elem.style.MozTransform = "rotate(" + degrees + "deg)";
+} else if (navigator.userAgent.match("MSIE")) {
+    elem.style.msTransform = "rotate(" + degrees + "deg)";
+} else if (navigator.userAgent.match("Opera")) {
+    elem.style.OTransform = "rotate(" + degrees + "deg)";
+} else {
+    elem.style.transform = "rotate(" + degrees + "deg)";
+}
+
+
+el.addEventListener("click", (e) => {  // КНОПКА СТАРТ
+    bol = 1;
+
+    elem1.style.height = (String(right_spring_height) + "px");
+    elem2.style.height = (String(left_spring_height) + "px");
+    // right_spring_height = 423;
+    // left_spring_height = 422;
+
+    if (first_weight.parentNode.id === "weight2") {
+        r1 = first_weight.parentNode.parentNode.id.slice(-1);
+        r2 = second_weight.parentNode.id.slice(-1);
+    } else if (second_weight.parentNode.id === "weight1") {
+        r1 = first_weight.parentNode.id.slice(-1);
+        r2 = second_weight.parentNode.parentNode.id.slice(-1);
+    } else {
+        r1 = first_weight.parentNode.id.slice(-1);
+        r2 = second_weight.parentNode.id.slice(-1);
+    }
+
+    //console.log(typeof(r1));
+
+
+    flag1 = 1;
+    switch (r1 + r2) {
+
+        case "00":
+            Tau = 5.21;
+            q = 7.44;
+            weight_radius_1 = 0;
+            weight_radius_2 = 0;
+            break;
+        case "01":
+            Tau = 3.14;
+            q = 7.66;
+            weight_radius_1 = 0;
+            weight_radius_2 = 0.06;
+            break;
+        case "02":
+            Tau = 3.49;
+            q = 7.25;
+            weight_radius_1 = 0;
+            weight_radius_2 = 0.1;
+            break;
+        case "03":
+            Tau = 4.77;
+            q = 7.73;
+            weight_radius_1 = 0;
+            weight_radius_2 = 0.14;
+            break;
+        case "04":
+            Tau = 5.55;
+            q = 7.95;
+            weight_radius_1 = 0;
+            weight_radius_2 = 0.18;
+            break;
+
+        case "10":
+            Tau = 3.14;
+            q = 7.66;
+            weight_radius_1 = 0.06;
+            weight_radius_2 = 0;
+            break;
+        case "11":
+            Tau = 3.76;
+            q = 7.78;
+            weight_radius_1 = 0.06;
+            weight_radius_2 = 0.06;
+            break;
+        case "12":
+            Tau = 4.32;
+            q = 7.82;
+            weight_radius_1 = 0.06;
+            weight_radius_2 = 0.1;
+            break;
+        case "13":
+            Tau = 4.95;
+            q = 7.78;
+            weight_radius_1 = 0.06;
+            weight_radius_2 = 0.14;
+            break;
+        case "14":
+            Tau = 7.1;
+            q = 7.87;
+            weight_radius_1 = 0.06;
+            weight_radius_2 = 0.18;
+            break;
+
+        case "20":
+            Tau = 3.49;
+            q = 7.25;
+            weight_radius_1 = 0.1;
+            weight_radius_2 = 0;
+            break;
+        case "21":
+            Tau = 4.32;
+            q = 7.82;
+            weight_radius_1 = 0.1;
+            weight_radius_2 = 0.06;
+            break;
+        case "22":
+            Tau = 4.73;
+            q = 7.83;
+            weight_radius_1 = 0.1;
+            weight_radius_2 = 0.1;
+            break;
+        case "23":
+            Tau = 5.31;
+            q = 7.8;
+            weight_radius_1 = 0.1;
+            weight_radius_2 = 0.14;
+            break;
+        case "24":
+            Tau = 7.58;
+            q = 7.87;
+            weight_radius_1 = 0.1;
+            weight_radius_2 = 0.18;
+            break;
+
+        case "30":
+            Tau = 4.77;
+            q = 7.73;
+            weight_radius_1 = 0.14;
+            weight_radius_2 = 0;
+            break;
+        case "31":
+            Tau = 4.95;
+            q = 7.78;
+            weight_radius_1 = 0.14;
+            weight_radius_2 = 0.04;
+            break;
+        case "32":
+            Tau = 5.31;
+            q = 7.8;
+            weight_radius_1 = 0.14;
+            weight_radius_2 = 0.1;
+            break;
+        case "33":
+            Tau = 5.3;
+            q = 7.43;
+            weight_radius_1 = 0.14;
+            weight_radius_2 = 0.14;
+            break;
+        case "34":
+            Tau = 6.5;
+            q = 7.81;
+            weight_radius_1 = 0.14;
+            weight_radius_2 = 0.18;
+            break;
+
+        case "40":
+            Tau = 5.55;
+            q = 7.95;
+            weight_radius_1 = 0.18;
+            weight_radius_2 = 0;
+            break;
+        case "41":
+            Tau = 7.1;
+            q = 7.87;
+            weight_radius_1 = 0.18;
+            weight_radius_2 = 0.06;
+            break;
+        case "42":
+            Tau = 7.58;
+            q = 7.87;
+            weight_radius_1 = 0.18;
+            weight_radius_2 = 0.1;
+            break;
+        case "43":
+            Tau = 6.5;
+            q = 7.81;
+            weight_radius_1 = 0.18;
+            weight_radius_2 = 0.14;
+            break;
+        case "44":
+            Tau = 8.6;
+            q = 8.126;
+            weight_radius_1 = 0.18;
+            weight_radius_2 = 0.18;
+            break;
+
+
+        default:
+            flag1=0;
+            break;
+
+    }
+
+
+    el.disabled = true;
+    el.style.backgroundColor = ("#659365");
+
+    let date = new Date();
+    now = date.getTime() / 1000;
+    t2 = date.getTime() / 1000;
+
+    A=degrees;
+
+    // if (navigator.userAgent.match("Chrome")) {
+    //     elem.style.WebkitTransform = "rotate(" + -degrees + "deg)";
+    // } else if (navigator.userAgent.match("Firefox")) {
+    //     elem.style.MozTransform = "rotate(" + -degrees + "deg)";
+    // } else if (navigator.userAgent.match("MSIE")) {
+    //     elem.style.msTransform = "rotate(" + -degrees + "deg)";
+    // } else if (navigator.userAgent.match("Opera")) {
+    //     elem.style.OTransform = "rotate(" + -degrees + "deg)";
+    // } else {
+    //     elem.style.transform = "rotate(" + -degrees + "deg)";
+    // }
+    if (flag1===1)
+        rotateAnimation(".move", 15);
+})
+
+
+
+// ползунок
+var slider = document.getElementById("myRange");
+
+slider.oninput = function() {
+    degrees = -1*this.value;
+    left_spring_height = 422 * (1 - Math.sin(degrees * Math.PI / 180) / 5);
+    right_spring_height = 423 * (1 + Math.sin(degrees * Math.PI / 180) / 5);
+
+
+    elem.style.transform = "rotate(" + degrees + "deg)"
+    elem1.style.height = (String(right_spring_height) + "px");
+    elem2.style.height = (String(left_spring_height) + "px");
+
+
+}
+
+el_stop.addEventListener("click", () => {     // КНОПКА СБРОС
+    el.disabled = false;
+    el.style.backgroundColor = ('#88CC88');
+
+    degrees = 0
+    right_spring_height = 423;
+    left_spring_height = 422;
+
+    elem.style.transform = "rotate("+degrees+"deg)";
+})
+// let s = "hello";
+// console.log(s.slice(0, 3));
+
