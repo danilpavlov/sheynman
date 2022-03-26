fa8bbda375b1af73b38bc5fa79c752309fb02885
ElizabethOnTheBathroomFloor <danil-pavlovz@mail.ru>
2022-03-26 22:12:36 +0300

    5th_move : (remove "/public" in app.get)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/public', (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 app.listen(3333, () => {
