4639a30328f718a82d0717ac3b50bc9ea17180d8
ElizabethOnTheBathroomFloor <danil-pavlovz@mail.ru>
2022-03-27 10:26:32 +0300

    6th_move : (add host and port on server)

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
-app.listen(3333, () => {
-    console.log('Application listening on port 3333!');
+
+app.listen(port, host, function() {
+    console.log("Server started.......");
 });
 
 
